15508de576d5ebfad91fcbdde06567a759597441
Simeon Lees <simeon.lees@hotmail.com>
2015-02-17 16:05:47 +0000

    Some changes

---
+++ template/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $title; ?></title>
+	<meta name="description" content="<?php echo $description; ?>">
+	<meta name="keywords" content="leeds, university, formula, team">
+</head>
+<body>
+	<h1>yo</h1>
+
+</body>
