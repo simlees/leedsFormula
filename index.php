3574795ada5000b8c9393bad7bbacaa8e722f39e
Simeon Lees <simeon.lees@hotmail.com>
2015-02-17 18:02:11 +0000

    Updated header

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php include 'template/header.php'; ?>
+<?php 
+$title = 'Leeds Formula';
+$description = 'Welcome to the home page for Leeds Formula';
+include 'template/header.php'; 
+
+?>
+
+<h1><span class="welcome">Welcome</span><br> To the Official Homepage of the Leeds Formula Race Team.</h1>
--- template/header.php
+++ template/header.php
@@ -4,8 +4,23 @@
 	<title><?php echo $title; ?></title>
 	<meta name="description" content="<?php echo $description; ?>">
 	<meta name="keywords" content="leeds, university, formula, team">
+
+	<!-- BOOTSTRAP -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+	<!-- STYLESHEETS -->
+
+
 </head>
 <body>
-	<h1>yo</h1>
+	<nav class="main-nav">
+		<ul>
+			<li>Home</li>
+			<li>Team</li>
+			<li>News</li>
+			<li>Heritage</li>
+			<li>Contact</li>
+		</ul>
+	</nav>
 
 </body>
